1b4a6a04bc85e784744f10a9c1c89b25ea62694b
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-16 23:10:54 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where('rating == ?', Show.highest_rating)
+    Show.where('rating == ?', Show.highest_rating).first
   end
 
   def self.lowest_rating
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.where('rating == ?', Show.lowest_rating)
+    Show.where('rating == ?', Show.lowest_rating).first
   end
 
   def self.ratings_sum
